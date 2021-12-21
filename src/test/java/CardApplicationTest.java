59ebe005e77b5d50a16dc0d93be20d62225c91aa
Olga Lebedeva <birdswann@gmail.com>
2021-12-21 17:00:53 +0300

    Webdriver manager library added

--- src/test/java/CardApplicationTest.java
+++ src/test/java/CardApplicationTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +17,7 @@ public class CardApplicationTest {
 
     @BeforeAll
     public static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
